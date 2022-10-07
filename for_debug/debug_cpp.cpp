0c555163201ca546e40486563e576b2e039f412f
PeOpLeRu <100120835+PeOpLeRu@users.noreply.github.com>
2022-10-07 22:10:03 +0800

    update

--- for_debug/debug_cpp.cpp
+++ for_debug/debug_cpp.cpp
@@ -1,23 +1,42 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
+/*
+int CRC_hash(const uint8_t* data, const int size)
+{
+    int hash = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        unsigned int highorder = hash & 0xf8000000;
+        hash = hash << 5;
+        hash = hash ^ (highorder >> 27);
+        hash = hash ^ data[i];
+    }
+
+    return hash;
+}
+*/
 
 int CRC_hash(const uint8_t* data, const int size)
 {
-  int hash = 0;
-  for (int i = 0; i < size; ++i)
-  {
-    int highorder = hash & 0xf8000000;
-    hash = hash << 5;
-    hash = hash ^ (highorder >> 27);
-    hash = hash ^ data[i];
-  } 
+    int hash = 0;
+    unsigned int highorder = 0;
 
-  return hash;
+    for (int i = 0; i < size; ++i)
+    {
+        highorder = hash & 0xf8000000;
+        hash = hash << 5;
+        hash = hash ^ (highorder >> 27);
+        hash = hash ^ data[i];
+    }
+
+    return hash;
 }
 
 int main()
-[
-    uint8_t data[] = { 3, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    cout << CRC_hash(data, 13);
-]
+{
+    uint8_t data[] = { 3,   1,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+ };
+    int hash = CRC_hash(data, 13);
+    cout << ((hash >> 24) & 0xFF) << ", " << ((hash >> 16) & 0xFF) << ", " << ((hash >> 8) & 0xFF) << ", " << (hash & 0xFF);
+}
